67fe72f446fbd98795ea0f90911208bf8d318184
Joel Davis <davisbrostechnologies@yahoo.com>
2021-07-25 15:50:08 -0400

    Added Global Variable class.
    These variables will be used as a scorecard.

---
+++ csharp/Tennis/GlobalVariables.cs
@@ -0,0 +1,10 @@
+﻿namespace Tennis
+{
+    class GlobalVariables
+    {
+        /// <summary>
+        /// Possible scores for tennis.
+        /// </summary>
+        public string[] score = { "Love-All", "Fifteen-All", "Thirty-All", "Deuce", "Fifteen-Love", "Love-Fifteen", "Thirty-Love", "Love-Thirty", "Forty-Love", "Love-Forty", "Win for player1", "Win for player2", "Thirty-Fifteen", "Fifteen-Thirty", "Forty-Fifteen", "Fifteen-Forty", "Forty-Thirty", "Thirty-Forty", "Win for player1", "Win for player2", "Advantage player1", "Advantage player2" };
+	}
+}
