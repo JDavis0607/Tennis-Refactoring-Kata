8e3a0d9bec97fd44a8a4feac3cd293bb050460eb
Joel Davis <davisbrostechnologies@yahoo.com>
2021-07-25 22:39:43 -0400

    Added player2 and improved score assignments.
    Replaced string values with global variables array values.

--- csharp/Tennis/TennisGame.cs
+++ csharp/Tennis/TennisGame.cs
@@ -19,7 +19,7 @@ namespace Tennis
         {
             if (playerName == "player1")
                 m_score1 += 1;
-            else
+            else if (playerName == "player2")
                 m_score2 += 1;
         }
 
@@ -29,37 +29,28 @@ namespace Tennis
             var tempScore = 0;
             if (m_score1 == m_score2)
             {
-                switch (m_score1)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
-                }
+                score = gv.score[m_score1];
             }
             else if (m_score1 >= 4 || m_score2 >= 4)
             {
                 var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+
+                if (minusResult == 1) score = gv.score[19];
+                else if (minusResult == -1) score = gv.score[20];
+                else if (minusResult >= 2) score = gv.score[17];
+                else score = gv.score[18];
             }
             else
             {
                 for (var i = 1; i < 3; i++)
                 {
                     if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
+                    else 
+                    {
+                        score += "-";
+                        tempScore = m_score2; 
+                    }
+
                     switch (tempScore)
                     {
                         case 0:
